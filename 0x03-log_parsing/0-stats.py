09c73cf00123737aa8704b82c51f0bfea1320f4d
Mario Raafat <mariorafat10@gmail.com>
2024-09-01 22:55:37 +0300

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-# Initialize variables
-total_size = 0
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-valid_status_codes = set(status_counts.keys())
-line_count = 0
-
-def print_statistics():
+def print_statistics(total_size, status_counts):
     """Prints the current statistics."""
     print(f"File size: {total_size}")
     for status_code in sorted(status_counts.keys()):
@@ -16,25 +10,32 @@ def print_statistics():
 
 def run():
     try:
+        total_size = 0
+        line_count = 0
+        status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+        valid_status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+
         for line in sys.stdin:
             # Strip whitespace and split the line into components
             parts = line.strip().split()
-
+            
             # Check if the line matches the expected format
-            if len(parts) < 7:
+            if len(parts) < 9:
                 continue
 
-            ip, dash, datetime_part, method, path, protocol, status, file_size = parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7]
+            status = parts[7]
+            file_size = parts[8]
 
             try:
                 # Convert status and file size to integers
-                status = int(status)
+                #status = int(status)
                 file_size = int(file_size)
 
                 # Update total file size and status code count if status is valid
                 total_size += file_size
                 if status in valid_status_codes:
-                    status_counts[status] += 1
+                    status_counts[int(status)] += 1
+
             except ValueError:
                 continue  # Skip the line if conversion fails
 
@@ -43,15 +44,15 @@ def run():
 
             # Print statistics every 10 lines
             if line_count % 10 == 0:
-                print_statistics()
+                print_statistics(total_size, status_counts)
 
     except KeyboardInterrupt:
         # Handle keyboard interrupt (CTRL + C)
-        print_statistics()
+        print_statistics(total_size, status_counts)
         sys.exit(0)
 
     # Print final statistics if the end of the input is reached
-    print_statistics()
+    print_statistics(total_size, status_counts)
 
 if __name__ == '__main__':
     run()

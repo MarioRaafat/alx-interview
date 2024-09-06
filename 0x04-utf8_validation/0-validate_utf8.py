7a11018f9ef7132c18946317d6986e8826606c78
Mario Raafat <136023677+MarioRaafat@users.noreply.github.com>
2024-09-06 03:34:13 +0300

    Copied

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,53 +1,61 @@
-#!/usr/bin/env python3
-"""
-This module contains a function to validate if a given dataset represents
-a valid UTF-8 encoding.
+#!/usr/bin/python3
+"""UTF-8 validation module.
 """
 
-from typing import List
 
-def validUTF8(data: List[int]) -> bool:
+def validUTF8(data):
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    Determine if a given dataset represents a valid UTF-8 encoding.
-
-    Args:
-        data (List[int]): A list of integers where each integer represents 1 byte.
-
-    Returns:
-        bool: True if data is a valid UTF-8 encoding, else False.
-    """
-    # Number of bytes in the current UTF-8 character
-    n_bytes = 0
-
-    # Masks to check the most significant bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
-
-    for num in data:
-        # Get the binary representation of the number, taking only the last 8 bits
-        bin_rep = num & 0xFF
-
-        if n_bytes == 0:
-            # Count the number of leading 1s to determine the number of bytes in the character
-            mask = 1 << 7
-            while mask & bin_rep:
-                n_bytes += 1
-                mask = mask >> 1
-
-            # 1-byte character or 0 leading 1s (valid case)
-            if n_bytes == 0:
-                continue
-
-            # If the number of bytes is more than 4 or 1 (invalid cases)
-            if n_bytes == 1 or n_bytes > 4:
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            # 4-byte utf-8 character encoding
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11110000 == 0b11100000:
+            # 3-byte utf-8 character encoding
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            # 2-byte utf-8 character encoding
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
                 return False
         else:
-            # Check if the most significant bits are '10'
-            if not (bin_rep & mask1 and not (bin_rep & mask2)):
-                return False
-        
-        # Decrease the number of bytes to process
-        n_bytes -= 1
-
-    # If there are no remaining bytes to process, the data is valid UTF-8
-    return n_bytes == 0
+            return False
+    return True

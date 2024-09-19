a76c2a5d32a02fac8d6e8d0b4e2c073da670470b
Mario Raafat <mariorafat10@gmail.com>
2024-09-19 05:19:13 +0300

    ya rb

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,43 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
+// Get the Movie ID from the command-line arguments
+const movieId = process.argv[2];
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+if (!movieId) {
+  console.error('Please provide a Movie ID');
+  process.exit(1);
 }
+
+// Star Wars API URL for the given movie
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+// Make a request to the Star Wars API
+request(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch the movie details. Status Code:', response.statusCode);
+    return;
+  }
+
+  // Get the characters list from the response body
+  const characters = body.characters;
+
+  // For each character, make a request to fetch the name
+  characters.forEach((characterUrl) => {
+    request(characterUrl, { json: true }, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error('Error fetching character:', charError);
+        return;
+      }
+
+      if (charResponse.statusCode === 200) {
+        console.log(charBody.name); // Print the character name
+      }
+    });
+  });
+});
